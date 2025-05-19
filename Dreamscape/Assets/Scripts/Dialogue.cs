6936ca5bf86ae2e1b3007b6b45adfe45088a2ac3
priyankapather <priyankapather@gmail.com>
2025-05-19 19:39:42 +0200

    dialogue and choices
    did dialogue
    choices
    graphics
    animatiosns
    all for chapter 1 excluding the cut scene

--- Dreamscape/Assets/Scripts/Dialogue.cs
+++ Dreamscape/Assets/Scripts/Dialogue.cs
@@ -38,24 +38,49 @@ using TMPro;
         public string[] lines;
         public string[] Actors;
         public float textSpeed;
-        public GameObject button1, button2;
-       
+        public GameObject button1, button2, button3,button4,button5,button6,button7,button8,button9,button10,button11,button12,button13,button14,button15,button16,button17,button18,button19,button20,button21,button22,button23;
+        
 
 
 
-    private int index;
+
+        private int index;
         private int actors;
 
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-           
+
         //textComponent.text = string.Empty;
+        //code to hide the buttons at the start of the game
             button1.gameObject.SetActive(false);
             button2.gameObject.SetActive(false);
-            //textComponent.text = string.Empty;
-            textComponent.text = string.Empty;
+            button3.gameObject.SetActive(false);
+            button4.gameObject.SetActive(false);
+            button5.gameObject.SetActive(false);
+            button6.gameObject.SetActive(false);
+            button7.gameObject.SetActive(false);
+            button8.gameObject.SetActive(false);
+            button9.gameObject.SetActive(false);
+            button10.gameObject.SetActive(false);
+            button11.gameObject.SetActive(false);
+            button12.gameObject.SetActive(false);
+            button13.gameObject.SetActive(false);
+            button14.gameObject.SetActive(false);
+            button15.gameObject.SetActive(false);
+            button16.gameObject.SetActive(false);
+            button17.gameObject.SetActive(false);
+            button18.gameObject.SetActive(false);
+            button19.gameObject.SetActive(false);
+            button20.gameObject.SetActive(false);
+            button21.gameObject.SetActive(false);
+            button22.gameObject.SetActive(false);
+            button23.gameObject.SetActive(false);
+            
+
+        //textComponent.text = string.Empty;
+        textComponent.text = string.Empty;
             nameTextComponent.text = string.Empty;
             StartDialogue();
 
@@ -91,17 +116,208 @@ using TMPro;
             }
 
             if(index == 11)
+            {
+                button1.gameObject.SetActive(true);
+                button2.gameObject.SetActive(true);
+            
+            }
+            else
+            {
+                button1.gameObject.SetActive(false);
+                button2.gameObject.SetActive(false);
+            
+            }
+
+            if (index == 11)
+            {
+                button3.gameObject.SetActive(true);
+            }
+            else
+            {
+                button3.gameObject.SetActive(false);
+            }
+
+            if (index == 10)
+            {
+            button4.gameObject.SetActive(true);
+            }
+            else
+            {
+            button4.gameObject.SetActive(false);
+            }
+
+            if (index == 4)
+            {
+            button5.gameObject.SetActive(true);
+            }
+            else
+            {
+            button5.gameObject.SetActive(false);
+            }
+
+            if (index == 4)
+            {
+            button6.gameObject.SetActive(true);
+            }
+            else
+            {
+            button6.gameObject.SetActive(false);
+            }
+
+            if(index == 8)
+            {
+            button7.gameObject.SetActive(true);
+            }
+            else
+            {
+            button7.gameObject.SetActive(false);
+            }
+
+             if (index == 8)
+            {
+            button8.gameObject.SetActive(true);
+            }
+             else
+            {
+            button8.gameObject.SetActive(false);
+            }
+
+            if (index == 2)
+            {
+                button9.gameObject.SetActive(true);
+            }
+            else
+            {
+                button9.gameObject.SetActive(false);
+            }
+
+            if (index == 2)
+            {
+                button10.gameObject.SetActive(true);
+            }
+            else
+            {
+                button10.gameObject.SetActive(false);
+            }
+
+            if (index == 8)
+            {
+                button11.gameObject.SetActive(true);
+            }
+            else
+            {
+                button11.gameObject.SetActive(false);
+            }
+
+            if (index == 6)
+            {
+                button12.gameObject.SetActive(true);
+            }
+            else
+            {
+                button12.gameObject.SetActive(false);
+            }
+
+            if (index == 8)
+            {
+                button13.gameObject.SetActive(true);
+            }
+            else
+            {
+                button13.gameObject.SetActive(false);
+            }
+
+            if (index == 8)
+            {
+                button14.gameObject.SetActive(true);
+            }
+            else
+            {
+                button14.gameObject.SetActive(false);
+            }
+
+            if (index == 6)
+            {
+                button15.gameObject.SetActive(true);
+            }
+            else
+            {
+                button15.gameObject.SetActive(false);
+            }
+
+            if (index == 16)
+            {
+                button16.gameObject.SetActive(true);
+            }
+            else
+            {
+                button16.gameObject.SetActive(false);
+            }
+
+        if (index == 16)
         {
-            button1.gameObject.SetActive(true);
-            button2.gameObject.SetActive(true);
+            button17.gameObject.SetActive(true);
         }
         else
         {
-            button1.gameObject.SetActive(false);
-            button2.gameObject.SetActive(false);
+            button17.gameObject.SetActive(false);
+        }
+
+        if (index == 6)
+        {
+            button18.gameObject.SetActive(true);
+        }
+        else
+        {
+            button18.gameObject.SetActive(false);
+        }
+
+        if (index == 6)
+        {
+            button19.gameObject.SetActive(true);
+        }
+        else
+        {
+            button19.gameObject.SetActive(false);
+        }
+
+        if (index == 6)
+        {
+            button20.gameObject.SetActive(true);
+        }
+        else
+        {
+            button20.gameObject.SetActive(false);
+        }
+
+        if (index == 12)
+        {
+            button21.gameObject.SetActive(true);
+        }
+        else
+        {
+            button21.gameObject.SetActive(false);
+        }
+
+        if (index == 6)
+        {
+            button22.gameObject.SetActive(true);
+        }
+        else
+        {
+            button22 .gameObject.SetActive(false);
+        }
+
+        if (index == 10)
+        {
+            button23.gameObject.SetActive(true);
+        }
+        else
+        {
+            button23.gameObject.SetActive(false);
         }
     }
-        void StartDialogue()
+    void StartDialogue()
         {
             index = 0;
             actors = 0;
@@ -129,6 +345,7 @@ using TMPro;
         {
             if (index < lines.Length)
             {
+                //elelmnt 0+1= elelmnt 1+1=element 2- switches btwn elements
                 index++;
                 //textComponent.text = string.Empty;
                 //StartCoroutine(TypeLine());
@@ -146,13 +363,10 @@ using TMPro;
         }
             else
             {
-                gameObject.SetActive(false);
+                //gameObject.SetActive(false);
             }
 
-            void choices()
-        {
-            //gameObject.SetActive(false);
-        }
+         
 
     }
     }
--- Dreamscape/Assets/Scripts/choices.cs
+++ Dreamscape/Assets/Scripts/choices.cs
@@ -9,12 +9,29 @@ public class DialogueSystem : MonoBehaviour
     [SerializeField] private TMP_Text[] choiceTexts;
     public GameObject dialoguebox1;
     public GameObject dialoguebox2;
-
-
+    public GameObject dialoguebox3;
+    public GameObject dialoguebox4;
+    public GameObject dialoguebox5;
+    public GameObject dialoguebox6;
+    public GameObject dialoguebox7;
+    public GameObject dialoguebox8;
+    public GameObject dialoguebox9;
+    public GameObject dialoguebox10;
+    public GameObject dialoguebox11;
+    public GameObject dialoguebox12;
+    public GameObject dialoguebox13;
+    public GameObject dialoguebox14;
+    public GameObject dialoguebox15;
+    public GameObject dialoguebox16;
+    public GameObject dialoguebox17;
+    public GameObject dialoguebox18;
+    public GameObject dialoguebox19;
+    public GameObject dialoguebox20;
+    public GameObject dialoguebox21;
     private int currentDialogueIndex = 0;
 
     // Sample dialogue structure
-    private DialogueNode[] dialogueTree = new DialogueNode[]
+    /*private DialogueNode[] dialogueTree = new DialogueNode[]
 
     {
         new DialogueNode(
@@ -36,19 +53,194 @@ public class DialogueSystem : MonoBehaviour
         )
         // Add more nodes as needed
 
-    };
+    };*/
 
     public void choice0()
     {
-     dialoguebox1.SetActive(false);
+        dialoguebox1.SetActive(false);
         dialoguebox2.SetActive(true);
-        
+
     }
 
-    void Start()
-    {
-        dialoguebox2.SetActive(false);
 
+    //set.Active means when you press the button 
+    public void choice1()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox2.SetActive(false);
+        dialoguebox3.SetActive(true);
+
+    }
+
+    public void choice2()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox3.SetActive(false);
+        dialoguebox4.SetActive(true);
+
+    }
+
+    public void choice3()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox4.SetActive(false);
+        dialoguebox5.SetActive(true);
+
+    }
+
+    public void choice4()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox5.SetActive(false);
+        dialoguebox6.SetActive(true);
+
+    }
+    public void choice5()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox6.SetActive(false);
+        dialoguebox7.SetActive(true);
+
+    }
+
+    public void choice6()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox7.SetActive(false);
+        dialoguebox8.SetActive(true);
+
+    }
+
+    public void choice7()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox8.SetActive(false);
+        dialoguebox9.SetActive(true);
+
+    }
+    public void choice8()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox9.SetActive(false);
+        dialoguebox10.SetActive(true);
+
+    }
+    public void choice9()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox10.SetActive(false);
+        dialoguebox11.SetActive(true);
+
+    }
+
+    public void choice10()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox11.SetActive(false);
+        dialoguebox12.SetActive(true);
+
+    }
+    public void choice11()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox12.SetActive(false);
+        dialoguebox13.SetActive(true);
+
+    }
+
+    public void choice12()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox13.SetActive(false);
+        dialoguebox14.SetActive(true);
+
+    }
+
+    public void choice13()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox14.SetActive(false);
+        dialoguebox15.SetActive(true);
+
+    }
+
+    public void choice14()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox15.SetActive(false);
+        dialoguebox16.SetActive(true);
+
+    }
+
+    public void choice15()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox16.SetActive(false);
+        dialoguebox17.SetActive(true);
+
+    }
+
+    public void choice16()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox17.SetActive(false);
+        dialoguebox18.SetActive(true);
+
+    }
+
+    public void choice17()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox18.SetActive(false);
+        dialoguebox19.SetActive(true);
+
+    }
+
+    public void choice18()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox19.SetActive(false);
+        dialoguebox20.SetActive(true);
+
+    }
+
+    public void choice19()
+    //code for a button click 
+    {
+        //moving from the 2nd panel to the 3rd panel at the click of a button 
+        dialoguebox20.SetActive(false);
+        dialoguebox21.SetActive(true);
+
+    }
+
+
+
+
+
+
+
+
+
+    /*void Start()
+    {
         // Hide all choice buttons initially
         foreach (Button button in choiceButtons)
         {
@@ -94,20 +286,21 @@ public class DialogueSystem : MonoBehaviour
             button.gameObject.SetActive(false);
         }
     }
-}
+}*/
 
-// Helper class to store dialogue data
-[System.Serializable]
-public class DialogueNode
-{
-    public string dialogueText;
-    public string[] choices;
-    public int[] nextNodeIndices;
-
-    public DialogueNode(string text, string[] choices, int[] nextIndices)
+    // Helper class to store dialogue data
+    [System.Serializable]
+    public class DialogueNode
     {
-        this.dialogueText = text;
-        this.choices = choices;
-        this.nextNodeIndices = nextIndices;
+        public string dialogueText;
+        public string[] choices;
+        public int[] nextNodeIndices;
+
+        public DialogueNode(string text, string[] choices, int[] nextIndices)
+        {
+            this.dialogueText = text;
+            this.choices = choices;
+            this.nextNodeIndices = nextIndices;
+        }
     }
 }
