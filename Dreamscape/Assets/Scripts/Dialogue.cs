3191b9a66671763500009c7ca401037c1bf8ca28
priyankapather <priyankapather@gmail.com>
2025-06-12 17:30:01 +0200

    Revert "chapter 2 and cutsceness"
    This reverts commit 695e7dffad88f6157a4ac39a984eb3442031ca00.

--- Dreamscape/Assets/Scripts/Dialogue.cs
+++ Dreamscape/Assets/Scripts/Dialogue.cs
@@ -2,28 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 
 
 
 //public class DialogueTrigger : MonoBehaviour
 //{
-//public Message[] messages;
-//public Actor[] actors;
+    //public Message[] messages;
+    //public Actor[] actors;
 
-//[System.Serializable]
-/*public class Message
-{
-    public int actorId;
-    public string message;
+    //[System.Serializable]
+    /*public class Message
+    {
+        public int actorId;
+        public string message;
 
-}
-[System.Serializable]
-public class Actor
-{
-    public string name;
-    public Sprite sprite;
-}
+    }
+    [System.Serializable]
+    public class Actor
+    {
+        public string name;
+        public Sprite sprite;
+    }
 
 
 }*/
@@ -32,20 +31,20 @@ public class Actor
 
 
 
-public class Dialogue : MonoBehaviour
+    public class Dialogue : MonoBehaviour
     {
         public TextMeshProUGUI textComponent;
         public TextMeshProUGUI nameTextComponent;
         public string[] lines;
         public string[] Actors;
         public float textSpeed;
-        public GameObject button1, button2, button3,button4,button5,button6,button7,button8,button9,button10,button11,button12,button13,button14;
+        public GameObject button1, button2, button3,button4,button5,button6,button7,button8,button9,button10,button11,button12,button13,button14,button15,button16,button17,button18,button19,button20,button21,button22,button23;
+        
 
 
 
 
-
-    private int index;
+        private int index;
         private int actors;
 
 
@@ -69,7 +68,17 @@ public class Dialogue : MonoBehaviour
             button12.gameObject.SetActive(false);
             button13.gameObject.SetActive(false);
             button14.gameObject.SetActive(false);
+            button15.gameObject.SetActive(false);
+            button16.gameObject.SetActive(false);
+            button17.gameObject.SetActive(false);
+            button18.gameObject.SetActive(false);
+            button19.gameObject.SetActive(false);
+            button20.gameObject.SetActive(false);
+            button21.gameObject.SetActive(false);
+            button22.gameObject.SetActive(false);
+            button23.gameObject.SetActive(false);
             
+
         //textComponent.text = string.Empty;
         textComponent.text = string.Empty;
             nameTextComponent.text = string.Empty;
@@ -106,7 +115,7 @@ public class Dialogue : MonoBehaviour
                 }
             }
 
-            if(index == 16)
+            if(index == 11)
             {
                 button1.gameObject.SetActive(true);
                 button2.gameObject.SetActive(true);
@@ -119,7 +128,7 @@ public class Dialogue : MonoBehaviour
             
             }
 
-            if (index == 6)
+            if (index == 11)
             {
                 button3.gameObject.SetActive(true);
             }
@@ -128,7 +137,7 @@ public class Dialogue : MonoBehaviour
                 button3.gameObject.SetActive(false);
             }
 
-            if (index == 4)
+            if (index == 10)
             {
             button4.gameObject.SetActive(true);
             }
@@ -137,7 +146,7 @@ public class Dialogue : MonoBehaviour
             button4.gameObject.SetActive(false);
             }
 
-            if (index == 15)
+            if (index == 4)
             {
             button5.gameObject.SetActive(true);
             }
@@ -146,7 +155,7 @@ public class Dialogue : MonoBehaviour
             button5.gameObject.SetActive(false);
             }
 
-            if (index == 15)
+            if (index == 4)
             {
             button6.gameObject.SetActive(true);
             }
@@ -155,7 +164,7 @@ public class Dialogue : MonoBehaviour
             button6.gameObject.SetActive(false);
             }
 
-            if(index == 4)
+            if(index == 8)
             {
             button7.gameObject.SetActive(true);
             }
@@ -164,7 +173,7 @@ public class Dialogue : MonoBehaviour
             button7.gameObject.SetActive(false);
             }
 
-             if (index == 2)
+             if (index == 8)
             {
             button8.gameObject.SetActive(true);
             }
@@ -191,7 +200,7 @@ public class Dialogue : MonoBehaviour
                 button10.gameObject.SetActive(false);
             }
 
-            if (index == 2)
+            if (index == 8)
             {
                 button11.gameObject.SetActive(true);
             }
@@ -200,7 +209,7 @@ public class Dialogue : MonoBehaviour
                 button11.gameObject.SetActive(false);
             }
 
-            if (index == 2)
+            if (index == 6)
             {
                 button12.gameObject.SetActive(true);
             }
@@ -209,7 +218,7 @@ public class Dialogue : MonoBehaviour
                 button12.gameObject.SetActive(false);
             }
 
-            if (index == 2)
+            if (index == 8)
             {
                 button13.gameObject.SetActive(true);
             }
@@ -218,7 +227,7 @@ public class Dialogue : MonoBehaviour
                 button13.gameObject.SetActive(false);
             }
 
-            if (index == 4)
+            if (index == 8)
             {
                 button14.gameObject.SetActive(true);
             }
@@ -227,7 +236,86 @@ public class Dialogue : MonoBehaviour
                 button14.gameObject.SetActive(false);
             }
 
-        
+            if (index == 6)
+            {
+                button15.gameObject.SetActive(true);
+            }
+            else
+            {
+                button15.gameObject.SetActive(false);
+            }
+
+            if (index == 16)
+            {
+                button16.gameObject.SetActive(true);
+            }
+            else
+            {
+                button16.gameObject.SetActive(false);
+            }
+
+        if (index == 16)
+        {
+            button17.gameObject.SetActive(true);
+        }
+        else
+        {
+            button17.gameObject.SetActive(false);
+        }
+
+        if (index == 6)
+        {
+            button18.gameObject.SetActive(true);
+        }
+        else
+        {
+            button18.gameObject.SetActive(false);
+        }
+
+        if (index == 6)
+        {
+            button19.gameObject.SetActive(true);
+        }
+        else
+        {
+            button19.gameObject.SetActive(false);
+        }
+
+        if (index == 6)
+        {
+            button20.gameObject.SetActive(true);
+        }
+        else
+        {
+            button20.gameObject.SetActive(false);
+        }
+
+        if (index == 12)
+        {
+            button21.gameObject.SetActive(true);
+        }
+        else
+        {
+            button21.gameObject.SetActive(false);
+        }
+
+        if (index == 6)
+        {
+            button22.gameObject.SetActive(true);
+        }
+        else
+        {
+            button22 .gameObject.SetActive(false);
+        }
+
+        if (index == 10)
+        {
+            button23.gameObject.SetActive(true);
+        }
+        else
+        {
+            button23.gameObject.SetActive(false);
+        }
     }
     void StartDialogue()
         {
